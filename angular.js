ebb602f78fa128ee3db5f8bb0f8001ca5c6167d2
Anusha-Kopparapu <30928436+Anusha-Kopparapu@users.noreply.github.com>
2017-10-25 19:30:40 +0530

    POC for masterpage layout

---
+++ angular.js
@@ -0,0 +1,12 @@
+var app=angular.module('userApp',['ngRoute']);
+app.config(['$routeProvider',function ($routeProvider) {
+    $routeProvider
+    .when("/anusha", {
+            templateUrl: "hfpage.html"
+             // controller: "profileCont"
+         })
+    .when("/bangaru", {
+            templateUrl: "hfpage.html"
+             // controller: "profileCont"
+         }); 
+}]);
