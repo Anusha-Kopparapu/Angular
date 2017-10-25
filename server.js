99acc8f2b9f45daed5b381cfce0dd142ce071e57
Anusha-Kopparapu <30928436+Anusha-Kopparapu@users.noreply.github.com>
2017-10-25 17:40:48 +0530

    angular_layout

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-// server.js
-const path = require('path');
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/angular-layout/index.html'));
+
+
+/*var express =require ('express');
+var app = express();
+
+
+app.use(express.static(__dirname+"/angular-layout"));
+app.listen(process.env.PORT||3000);
+console.log('server is runnig on 3000');
+*/
+
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
+
+
